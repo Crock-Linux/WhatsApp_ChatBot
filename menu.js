32589e9d7ba3efae63a9dffd7f9411c3019e407e
Gabriel Lemos <gabriel_lemos06@hotmail.com>
2021-04-16 20:45:29 -0300

    testando os estados do usuaario

--- menu.js
+++ menu.js
@@ -1,15 +1,46 @@
 const venom = require('venom-bot');
 const textos = require('./manifest.json');
+const dataBase = require('./Database/data')
 
 const menu = function(client, message) {  
-    venom
-    client
-    .sendText(message.from, textos.Menu)
-    .then((result) => {
-        console.log('Result: ', result); //return object success
-    })
-    .catch((erro) => {
-        console.error('Error when sending: ', erro); //return object error
-    });
-}
+
+    if(dataBase.find(c => c.user == message.from)){
+        const user = dataBase.find(c => c.user == message.from);
+        if (user.state === 'menu' && message.body == '1') {
+            client
+            .sendText(message.from, textos.opcoes.terceira)
+            .then((result) => {
+                console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+            });
+            user.state = 'suporte'
+            console.log(dataBase)   
+        }
+    } else {
+        let user = {
+
+        }
+        user.user = message.from;
+        user.state = 'menu';
+        dataBase.push(user);
+        console.log(user);
+        console.log('não achou')
+        console.log(dataBase.find(c => c.user == message.from))
+        client
+        .sendText(message.from, textos.Menu)
+        .then((result) => {
+            console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+        });
+    }
+
+        
+    
+};
+
+
 module.exports = menu;
